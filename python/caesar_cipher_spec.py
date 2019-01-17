97dafe8090b99d7aec0b882b8d5b4c10d944910b
jalletto <joshalletto@gmail.com>
2019-01-17 13:18:08 -0600

    change puts to print

--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -1,5 +1,5 @@
 from caesar_cipher import caesar_cipher
 
-puts(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
-puts(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
-puts(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
+print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
+print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
+print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
