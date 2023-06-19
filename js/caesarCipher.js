0b86df5892d13a165d3c5e0614ec715f7e31a799
EC2 Default User <yeexiong@users.noreply.github.com>
2023-06-19 17:18:49 +0000

    caesar-cipher finished

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,34 +1,34 @@
 exports.caesarCipher = function(str, shift) {
-  var chars = str.split('');
-  var uppercaseStart = 'A'.charCodeAt(0);
-  var lowercaseStart = 'a'.charCodeAt(0);
+  var chars = str.split(''); // Split the string into an array of characters
+  var uppercaseStart = 'A'.charCodeAt(0); // Get the character code for 'A'
+  var lowercaseStart = 'a'.charCodeAt(0); // Get the character code for 'a'
 
   for (var i = 0; i < chars.length; i++) {
-    var char = chars[i];
-    var code = char.charCodeAt(0);
+    var char = chars[i]; // Get the current character
+    var code = char.charCodeAt(0); // Get the character code of the current character
 
-    if (isAlphabetic(char)) {
-      var isUpperCase = isUpperCaseLetter(code);
+    if (isAlphabetic(char)) { // Check if the character is alphabetic
+      var isUpperCase = isUpperCaseLetter(code); // Check if the character is uppercase
 
-      code = (code - (isUpperCase ? uppercaseStart : lowercaseStart) + shift) % 26;
-      if (code < 0) {
+      code = (code - (isUpperCase ? uppercaseStart : lowercaseStart) + shift) % 26; // Apply the Caesar cipher shift to the character code
+      if (code < 0) { // Handle wrapping around the alphabet
         code += 26;
       }
 
-      char = String.fromCharCode(code + (isUpperCase ? uppercaseStart : lowercaseStart));
+      char = String.fromCharCode(code + (isUpperCase ? uppercaseStart : lowercaseStart)); // Convert the modified character code back to a character
     }
 
-    chars[i] = char;
+    chars[i] = char; // Update the character in the array
   }
 
-  return chars.join('');
+  return chars.join(''); // Convert the array back to a string and return it
 };
 
 function isAlphabetic(char) {
-  var code = char.charCodeAt(0);
-  return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+  var code = char.charCodeAt(0); // Get the character code
+  return (code >= 65 && code <= 90) || (code >= 97 && code <= 122); // Check if the character is within the range of uppercase or lowercase letters
 }
 
 function isUpperCaseLetter(code) {
-  return code >= 65 && code <= 90;
+  return code >= 65 && code <= 90; // Check if the character code corresponds to an uppercase letter
 }
